09be1942eb15879cdbc7d9956b1a297254df4b2e
jonduffy <jon@3bpairs.com>
2023-06-07 18:54:14 +0100

    remove id

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -7,17 +7,9 @@ import { ServiceStack } from '../lib/service-stack';
 
 const app = new cdk.App();
 
-// new DNSStack(app, 'DNSStack', 'dev', {
-//   env: { account: '106225348621', region: 'eu-west-2' }
 
-// });
-
-// new NetworkStack(app, 'NetworkStack', "170.0.0.0/16", 'dev', {
-//   env: { account: '106225348621', region: 'eu-west-2' },
-  
-// });
 
 new ServiceStack(app, 'TaskStack', {
 
-  env: { account: '106225348621', region: 'eu-west-2' } 
+  env: { account: '*********', region: 'eu-west-2' } 
 });
